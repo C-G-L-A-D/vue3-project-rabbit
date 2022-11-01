caabdf51cc67f28b25a03552ae7781c200ed1174
lad <ladqmpw@163.com>
2022-11-01 14:07:00 +0800

    引入axios并进行封装，引入vuex持久化库

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,20 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+import user from './modules/user'
+import cart from './modules/cart'
+import category from './modules/category'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    user,
+    cart,
+    category
+  },
+  plugins: [
+    createPersistedState({
+      key: 'rabbit-store',
+      paths: ['user', 'cart']
+    })
+  ]
 })
