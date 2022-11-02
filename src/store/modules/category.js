00cf69565f7c5587bc14fec485b70d8572a9c926
lad <ladqmpw@163.com>
2022-11-02 22:56:22 +0800

    新增二季导航显示隐藏控制属性和方法

--- src/store/modules/category.js
+++ src/store/modules/category.js
@@ -14,6 +14,10 @@ export default {
     // 获取分类全部数据
     async getListActions (content) {
       const { result } = await getAllCategory()
+      // 给一级分类添加控制二级分类显示隐藏的标志
+      result.forEach(element => {
+        element.open = false
+      })
       console.log(result)
       content.commit('setList', result)
     }
@@ -22,6 +26,17 @@ export default {
     // 设置分类集合
     setList (state, payload) {
       state.list = payload
+    },
+    // 用于显示二级分类
+    showSubCategory (state, categoryId) {
+      // 只需要修改传进来的一级分类的open属性
+      const category = state.list.find(item => item.id === categoryId)
+      category.open = true
+    },
+    // 用于隐藏二级分类
+    hideSubCategory (state, categoryId) {
+      const category = state.list.find(item => item.id === categoryId)
+      category.open = false
     }
   }
 }
