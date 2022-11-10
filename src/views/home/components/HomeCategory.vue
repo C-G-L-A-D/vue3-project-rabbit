2a22310f4ab1274b4717708f2291c48f659058ee
lad <ladqmpw@163.com>
2022-11-10 21:23:49 +0800

    晚上品牌分类推荐

--- src/views/home/components/HomeCategory.vue
+++ src/views/home/components/HomeCategory.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="home-category">
+  <div class="home-category" @mouseleave="categoryId = null">
     <!-- 左侧分类 -->
     <ul class="menu">
       <li
         v-for="item in menuList"
         :key="item.id"
         @mouseenter="categoryId = item.id"
+        :class="{ active: categoryId === item.id }"
       >
         <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
         <template v-if="item.children">
@@ -21,18 +22,34 @@
     </ul>
     <!-- 分类推荐弹窗 -->
     <div class="layer">
-      <h4>分类推荐 <small>根据您的购买或浏览记录推荐</small></h4>
-      <ul v-if="curCategory && curCategory.goods && curCategory.goods.length">
+      <h4>
+        {{ curCategory && curCategory.id === "brand" ? "品牌" : "分类" }}推荐
+        <small>根据您的购买或浏览记录推荐</small>
+      </h4>
+      <!-- 商品 -->
+      <ul v-if="curCategory && curCategory.goods">
         <li v-for="item in curCategory.goods" :key="item.id">
           <RouterLink to="/">
-            <img
-              :src="item.picture"
-              :alt="`推荐商品-${item.name}`"
-            />
+            <img :src="item.picture" :alt="`推荐商品-${item.name}`" />
             <div class="info">
-              <p class="name ellipsis-2">{{item.name}}</p>
-              <p class="desc ellipsis">{{item.desc}}</p>
-              <p class="price"><i>¥</i>{{item.price}}</p>
+              <p class="name ellipsis-2">{{ item.name }}</p>
+              <p class="desc ellipsis">{{ item.desc }}</p>
+              <p class="price"><i>¥</i>{{ item.price }}</p>
+            </div>
+          </RouterLink>
+        </li>
+      </ul>
+      <!-- 品牌 -->
+      <ul v-if="curCategory && curCategory.brands">
+        <li class="brand" v-for="brand in curCategory.brands" :key="brand.id">
+          <RouterLink to="/">
+            <img :src="brand.picture" alt="" />
+            <div class="info">
+              <p class="place">
+                <i class="iconfont icon-dingwei"></i>{{ brand.place }}
+              </p>
+              <p class="name ellipsis">{{ brand.name }}</p>
+              <p class="desc ellipsis-2">{{ brand.desc }}</p>
             </div>
           </RouterLink>
         </li>
@@ -44,6 +61,7 @@
 <script>
 import { computed, reactive, ref } from 'vue'
 import { useStore } from 'vuex'
+import { findBrand } from '@/api/home'
 export default {
   name: 'HomeCategory',
   setup () {
@@ -57,9 +75,16 @@ export default {
           id: 'brand-children',
           name: '品牌推荐'
         }
-      ]
+      ],
+      // 品牌数据
+      brands: []
+    })
+    // 调用api获品牌推荐数据
+    findBrand().then((data) => {
+      brand.brands = data.result.slice(0, 6)
     })
 
+    // 获取所有分类数据
     const menuList = computed(() => {
       // 只需要获取每个一级分类的2个二级分类
       const list = store.state.category.list.map((item) => {
@@ -80,7 +105,7 @@ export default {
     const categoryId = ref(null)
     // 获取当前选择的分类
     const curCategory = computed(() => {
-      return menuList.value.find(item => item.id === categoryId.value)
+      return menuList.value.find((item) => item.id === categoryId.value)
     })
 
     return {
@@ -98,13 +123,13 @@ export default {
   height: 500px;
   background: rgba(0, 0, 0, 0.8);
   position: relative;
-  // 左侧分类分类
+  z-index: 99;
   .menu {
     li {
       padding-left: 40px;
       height: 50px;
       line-height: 50px;
-      &:hover {
+      &:hover,&.active {
         background: @xtxColor;
       }
       a {
@@ -116,17 +141,16 @@ export default {
       }
     }
   }
-  // 弹窗样式
-  .layer {
+  // 弹出层样式
+    .layer {
     width: 990px;
     height: 500px;
-    background: rgba(255, 255, 255, 0.8);
+    background: rgba(255,255,255,0.8);
     position: absolute;
     left: 250px;
     top: 0;
     display: none;
     padding: 0 15px;
-    text-align: left;
     h4 {
       font-size: 20px;
       font-weight: normal;
@@ -156,15 +180,15 @@ export default {
           height: 100%;
           align-items: center;
           padding: 10px;
+          text-align: left;
           &:hover {
             background: #e3f9f4;
           }
           img {
-            width: 95px;
-            height: 95px;
+              width: 95px;
+              height: 95px;
           }
           .info {
-            text-align: left;
             padding-left: 10px;
             line-height: 24px;
             width: 190px;
@@ -185,9 +209,27 @@ export default {
           }
         }
       }
+      // 品牌的样式
+      li.brand {
+        height: 180px;
+        a {
+          align-items: flex-start;
+          img {
+            width: 120px;
+            height: 160px;
+          }
+          .info {
+            p {
+              margin-top: 8px;
+            }
+            .place {
+              color: #999;
+            }
+          }
+        }
+      }
     }
   }
-  //   通过样式控制推荐弹层的显隐
   &:hover {
     .layer {
       display: block;
